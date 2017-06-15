1a9301651b6828b64217a2d0b4419c23350dc77b
cenap <salihcenap@gmail.com>
2017-06-15 15:43:48 +0300

    particle engine integrated

--- js/cparticles.js
+++ js/cparticles.js
@@ -1,39 +1,49 @@
 var DAMPING = 0.99;
-var draw = SVG('drawing');
 
-function Particle(x, y, w, h) {
+function Particle(x, y, z, w, h, d) {
   this.x = this.oldX = x;
   this.y = this.oldY = y;
-  this.r = this.rx = 6;
-  this.width=w;
-  this.height=h;
-  this.container = draw.nested();
-  this.container.move(x,y);
-  this.shape = this.container.rect(w, h);
-  this.color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  this.shape.attr({ fill: this.color });
+  this.z = this.oldZ = z;
+  var geometri = new THREE.BoxGeometry(w, h, d);
+  var materyal = new THREE.MeshPhongMaterial({color: 0x1000000+(Math.random())*0xffffff});
+  this.obj = new THREE.Mesh(geometri, materyal);
+  this.obj.position.x = x;
+  this.obj.position.y = y;
+  this.obj.position.z = z;
 }
 
 Particle.prototype.integrate = function() {
   var velocityX = (this.x - this.oldX) * DAMPING;
   var velocityY = (this.y - this.oldY) * DAMPING;
+  var velocityZ = (this.z - this.oldZ) * DAMPING;
   this.oldX = this.x;
   this.oldY = this.y;
+  this.oldZ = this.z;
   this.x += velocityX;
   this.y += velocityY;
+  this.z += velocityZ;
+  this.obj.position.x = this.x;
+  this.obj.position.y = this.y;
+  this.obj.position.z = this.z;
 };
 
 Particle.prototype.attract = function(x, y) {
   var dx = x - this.x;
   var dy = y - this.y;
-  var distance = Math.sqrt(dx * dx + dy * dy);
+  var dz = z - this.z;
+  var distance = Math.cbrt(dx * dx + dy * dy + dz * dz);
   this.x += dx / distance;
   this.y += dy / distance;
+  this.z += dz / distance;
+  this.obj.position.x = this.x;
+  this.obj.position.y = this.y;
+  this.obj.position.z = this.z;
 };
 
 Particle.prototype.rotate = function() {
-  this.r += this.rx;
-  this.shape.rotate(this.r);
+  this.obj.rotation.x += 0.01;
+  this.obj.rotation.y += 0.01;
+  this.obj.rotation.z += 0.01;
 };
 
 Particle.prototype.move = function() {
--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 ﻿
 var sahne = new THREE.Scene();
+var particles = [];
+var numberOfParticles = 15;
 var pg = document.getElementById('pg');
 var genişlik = pg.clientWidth; //window.innerWidth;
 var yükseklik = pg.clientHeight; //window.innerHeight;
@@ -8,6 +10,7 @@ var en_uzak = 1000;
 var bakış_açısı = 75;
 var aspect_ratio = genişlik / yükseklik;
 var kamera = new THREE.PerspectiveCamera(bakış_açısı, aspect_ratio, en_yakın, en_uzak);
+kamera.position.z = 200;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(genişlik, yükseklik);
@@ -22,31 +25,45 @@ var ışık = new THREE.PointLight(0xffffff);
 ışık.position.set(-250, 250, 250);
 sahne.add(ışık);
 
-var kutu_geometrisi = new THREE.BoxGeometry(2, 2, 2);
-var küp_materyali = new THREE.MeshPhongMaterial({
-  color: 0x00ff00
-});
-var küp = new THREE.Mesh(kutu_geometrisi, küp_materyali);
-sahne.add(küp);
+
+function init() {
+  for (var i = 0; i < numberOfParticles; i++) {
+    var size = Math.random() * 10 + 5;
+    particles[i] = new Particle(genişlik/20 - Math.random() * genişlik/10, yükseklik/20 - Math.random() * yükseklik/10, 0 , size, size, size);
+    sahne.add(particles[i].obj);
+  }
+}
+
+init();
 
 /*
 var küre_geometrisi = new THREE.SphereGeometry(1.3, 32, 16);
-var küre_materyali = new THREE.MeshPhongMaterial({
-  color: 0xffff00
-});
+var küre_materyali = new THREE.MeshPhongMaterial({color: 0xffff00});
 var küre = new THREE.Mesh(küre_geometrisi, küre_materyali);
 sahne.add(küre);
 */
 
-kamera.position.z = 5;
 
 var render = function() {
-  requestAnimationFrame(render);
 
-  küp.rotation.x += 0.01;
-  küp.rotation.y += 0.01;
+  for (var i = 0; i < numberOfParticles; i++) {
+    particles[i].integrate();
+    particles[i].rotate();
+  }
 
   renderer.render(sahne, kamera);
 };
 
-render();
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+(function animloop() {
+  requestAnimFrame(animloop);
+  render();
+})();
