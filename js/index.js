5c6cce7b7eb3377c3c4422bf2f3647c686e66e43
Salih Cenap Baydar <salihcenap@gmail.com>
2018-01-10 18:27:14 +0300

    bug-fixes

--- js/index.js
+++ js/index.js
@@ -11,45 +11,69 @@ var bakış_açısı = 75;
 var aspect_ratio = genişlik / yükseklik;
 var kamera = new THREE.PerspectiveCamera(bakış_açısı, aspect_ratio, en_yakın, en_uzak);
 var hedefx = 0, hedefy = 0, hedefz = 0, dz=1;
-kamera.position.z = 200;
+var renderer;
 
-pg.addEventListener('mousemove', onMousemove);
-pg.addEventListener('click', onClick);
-// IE9, Chrome, Safari, Opera
-pg.addEventListener("mousewheel", onMouseWheel, false);
-// Firefox
-//pg.addEventListener("DOMMouseScroll", onMouseWheel, false);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(genişlik, yükseklik);
-//document.body.appendChild(renderer.domElement);
 
-pg.appendChild(renderer.domElement);
+function initRenderer() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(genişlik, yükseklik);
+  renderer.setClearColor(0x202025);
+  renderer.sortObjects = true;
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  pg.appendChild(renderer.domElement);
+}
 
-var genel_ışık = new THREE.AmbientLight(0x202020); // soft white light
-sahne.add(genel_ışık);
+function initListeners() {
+  //window.addEventListener( 'resize', onWindowResize, false );
+  pg.addEventListener('mousemove', onMousemove);
+  pg.addEventListener('click', onClick);
+  // IE9, Chrome, Safari, Opera
+  pg.addEventListener("mousewheel", onMouseWheel, false);
+  // Firefox
+  pg.addEventListener("DOMMouseScroll", onMouseWheel, false);
+}
 
-var ışık = new THREE.PointLight(0xffffff);
-ışık.position.set(-250, 250, 250);
-sahne.add(ışık);
+function initLights() {
+  var genel_ışık = new THREE.AmbientLight(0x202020); // soft white light
+  sahne.add(genel_ışık);
 
+  var ışık = new THREE.PointLight(0xffffff);
+  ışık.position.set(-250, 250, 250);
+  sahne.add(ışık);
+}
+
+function initCamera() {
+  kamera.position.x = 0;
+  kamera.position.y = 0;
+  kamera.position.z = 200;
+  kamera.zoom = 3.0;
+  kamera.updateProjectionMatrix();
+}
+
+function initFog() {
+  sahne.fog = new THREE.Fog(0xffffff, 20, 100);
+  sahne.fog.color.setHSL(0.51, 0.6, 0.6);
+}
 
 function init() {
+  //var küre_geometrisi = new THREE.SphereGeometry(1.3, 32, 16);
+  var geometry = new THREE.BoxGeometry(2, 2, 2);
+  var material = new THREE.MeshPhongMaterial({color: 0xffff00});
   for (var i = 0; i < numberOfParticles; i++) {
     var size = Math.random() * 10 + 5;
-    particles[i] = new Particle(genişlik/2 - Math.random() * genişlik, yükseklik/2 - Math.random() * yükseklik, 0 , size, size, size);
+    particles[i] = new Particle(geometry, material, genişlik/2 - Math.random() * genişlik, yükseklik/2 - Math.random() * yükseklik, 0);
     sahne.add(particles[i].obj);
   }
 }
 
+initRenderer();
+initListeners();
+initLights();
+initCamera();
 init();
 
-/*
-var küre_geometrisi = new THREE.SphereGeometry(1.3, 32, 16);
-var küre_materyali = new THREE.MeshPhongMaterial({color: 0xffff00});
-var küre = new THREE.Mesh(küre_geometrisi, küre_materyali);
-sahne.add(küre);
-*/
 
 
 var render = function() {
@@ -64,10 +88,7 @@ var render = function() {
 };
 
 window.requestAnimFrame = (function() {
-  return window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    function(callback) {
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
       window.setTimeout(callback, 1000 / 60);
     };
 })();
